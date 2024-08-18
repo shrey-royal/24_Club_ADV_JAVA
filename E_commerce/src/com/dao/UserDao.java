9f35db1114371312916fcf2d0d96e23d37b42f25
Shrey K <srkkadia@gmail.com>
2024-08-18 17:14:30 +0530

    validateUser, password hash

--- E_commerce/src/com/dao/UserDao.java
+++ E_commerce/src/com/dao/UserDao.java
@@ -2,10 +2,13 @@ package com.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.bean.UserBean;
 import com.util.DBConnection;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 public class UserDao {
 	Connection conn = null;
@@ -19,12 +22,39 @@ public class UserDao {
 			pstmt = conn.prepareStatement(query);
 			pstmt.setString(1, user.getEmail());
 			pstmt.setString(2, user.getUsername());
-			pstmt.setString(3, user.getPassword());
+			
+			String hashedPassword = BCrypt.withDefaults().hashToString(12, user.getPassword().toCharArray());
+			
+			pstmt.setString(3, hashedPassword);
 			
 			return (pstmt.executeUpdate() == 1) ? true: false;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return false;
+	}
+	
+	public UserBean validateUser(String username, String password) {
+		String query = "SELECT * FROM users WHERE username = ?";
+		UserBean user = null;
+		try {
+			conn = DBConnection.getConnection();
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, username);
+			ResultSet rs = pstmt.executeQuery();
+			if(rs.next()) {
+				String storedHash = rs.getString("password");
+				BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), storedHash);
+
+				if(result.verified) {
+					user = new UserBean();
+					user.setUser_id(rs.getInt("user_id"));
+					user.setUsername(rs.getString("username"));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return user;
 	}
 }
